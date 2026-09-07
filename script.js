eef561641642695f427e2287b9fdedf3e2a7561d
Muhammed Mustafa Ayyıldız <muhammederendemirkiran@gmail.com>
2026-09-07 13:18:25 +0300

    buton ekleme, buton okumayı öğrendim, onun yanında bir butona bastığımda bir şeyler yazdırmayı öğrendim

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ const citiesByCountry = {
 const countryButtons = document.querySelectorAll('.country-btn');
 const cityList = document.getElementById('cityList');
 
+function clickCountryButton() {
+    const clikkedButton = event.currentTarget;
+    const country = clikkedButton.textContent;
+    const cities = citiesByCountry[country];
+
+    displayCities(cities);
+}
+countryButtons.forEach(function(button) {
+    button.addEventListener('click', clickCountryButton);//addEventListener ile clickCountryButton fonksiyonunu çağırıyor
+    console.log("Country button clicked");
+});
+function displayCities(cities){
+    cityList.innerHTML = ''; // önceki şehirleri temizliyormuş
+    cities.forEach(function(city){
+        const cityButton = document.createElement('button');
+        cityButton.textContent = city; //burası cityButton.textContent = city; //burası cityButton'un textContent'ini ayarlıyor
+        cityList.appendChild(cityButton); //burası cityList.appendChild(cityButton); //burası cityList'e ekliyor
+    });
+}
